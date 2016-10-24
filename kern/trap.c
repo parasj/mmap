08f1869718cad1d662f87a08850de80e93649179
Jay Kamat <jaygkamat@gmail.com>
2016-10-24 14:36:13 -0400

    Fix up traps

--- kern/trap.c
+++ kern/trap.c
@@ -123,13 +123,13 @@ trap_init_percpu(void)
   thiscpu->cpu_ts.ts_ss0 = GD_KD;
 
   // Initialize the TSS slot of the gdt.
-  gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t)(&(thiscpu->cpu_ts)),
+  gdt[(GD_TSS0 >> 3) + thiscpu->cpu_id] = SEG16(STS_T32A, (uint32_t)(&(thiscpu->cpu_ts)),
                             sizeof(struct Taskstate) - 1, 0);
-  gdt[GD_TSS0 >> 3].sd_s = 0;
+  gdt[(GD_TSS0 >> 3) + thiscpu->cpu_id].sd_s = 0;
 
   // Load the TSS selector (like other segment selectors, the
   // bottom three bits are special; we leave them 0)
-  ltr(GD_TSS0 + thiscpu->cpu_id * sizeof(struct Segdesc));
+  ltr(thiscpu->cpu_id * sizeof(struct Segdesc) + GD_TSS0);
 
   // Load the IDT
   lidt(&idt_pd);
