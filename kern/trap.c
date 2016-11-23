424fec612f6d45a5e6868be430cab97d63dce3bc
Jay Kamat <jaygkamat@gmail.com>
2016-11-23 12:04:22 -0500

    Add forgotten returns

--- kern/trap.c
+++ kern/trap.c
@@ -219,14 +219,17 @@ trap_dispatch(struct Trapframe *tf)
   if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
     lapic_eoi();
     sched_yield();
+    return;
   }
 
   // Handle keyboard and serial interrupts.
   // LAB 5: Your code here.
   if (tf->tf_trapno == IRQ_OFFSET + IRQ_KBD) {
     kbd_intr();
+    return;
   } else if (tf->tf_trapno == IRQ_OFFSET + IRQ_SERIAL) {
     serial_intr();
+    return;
   }
 
   // Unexpected trap: The user process or the kernel has a bug.
